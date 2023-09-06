0edc4c229f026368b50476059184ff6a40d4b9df
Rajath Pai <rpai@copado.com>
2023-09-06 16:10:05 +0530

    lwc commit 06Sep

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-  greeting = 'World dev1';
+  greeting = 'World dev1 06Sep';
   changeHandler(event) {
     this.greeting = event.target.value;
   }
